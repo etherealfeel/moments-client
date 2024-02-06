d8fbe59c15a4b08883418e4f9543ccd7a59b9058
etherealfeel <graciagodsyndrome@gmail.com>
2024-02-06 15:46:26 +0200

    Add create post action

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -2,7 +2,16 @@ import * as api from '../api';
 export const getPosts = () => async (dispatch) => {
     try {
         const { data } = await api.fetchPosts();
-        dispatch({ type: 'FETCH_ALL', data: [] });
+        dispatch({ type: 'FETCH_ALL', payload: data });
+    } catch (err) {
+        console.log(err.message);
+    }
+};
+
+export const createPost = (post) => async (dispatch) => {
+    try {
+        const { data } = await api.createPost(post);
+        dispatch({ type: 'CREATE_POST', payload: data });
     } catch (err) {
         console.log(err.message);
     }
