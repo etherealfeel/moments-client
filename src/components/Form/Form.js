9e47bfe24f0c154b1386baf260e7a842559bca9a
etherealfeel <graciagodsyndrome@gmail.com>
2024-02-07 10:29:41 +0200

    Apply post card styling

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,9 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useStyles from './styles';
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
+import FileBase64 from 'react-file-base64';
+import { useDispatch } from 'react-redux';
+import { createPost } from '../../actions/posts';
 
 const Form = () => {
+    const [postData, setPostData] = useState({
+        author: '',
+        title: '',
+        message: '',
+        tags: '',
+        selectedFile: ''
+    });
     const classes = useStyles();
-    return <h1>Form</h1>;
+    const dispatch = useDispatch();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(createPost(postData));
+    };
+    const handleClear = () => {};
+    return (
+        <Paper className={classes.paper}>
+            <form
+                className={`${classes.root} ${classes.form}`}
+                autoComplete="off"
+                noValidate
+                onSubmit={handleSubmit}
+            >
+                <Typography variant="h6">Create a Moment</Typography>
+                <TextField
+                    name="author"
+                    value={postData.author}
+                    variant="outlined"
+                    label="author"
+                    fullWidth
+                    onChange={(e) =>
+                        setPostData({ ...postData, author: e.target.value })
+                    }
+                />
+                <TextField
+                    name="title"
+                    value={postData.title}
+                    variant="outlined"
+                    label="Title"
+                    fullWidth
+                    onChange={(e) =>
+                        setPostData({ ...postData, title: e.target.value })
+                    }
+                />
+                <TextField
+                    name="message"
+                    value={postData.message}
+                    variant="outlined"
+                    label="Message"
+                    fullWidth
+                    onChange={(e) =>
+                        setPostData({ ...postData, message: e.target.value })
+                    }
+                />
+                <TextField
+                    name="tags"
+                    value={postData.tags}
+                    variant="outlined"
+                    label="Tags"
+                    fullWidth
+                    onChange={(e) =>
+                        setPostData({ ...postData, tags: e.target.value })
+                    }
+                />
+                <div className={classes.fileInput}>
+                    <FileBase64
+                        type="file"
+                        multiple={false}
+                        onDone={({ base64 }) =>
+                            setPostData({ ...postData, selectedFile: base64 })
+                        }
+                    />
+                </div>
+                <Button
+                    className={classes.buttonSubmit}
+                    variant="outlined"
+                    color="primary"
+                    size="large"
+                    type="submit"
+                    fullWidth
+                >
+                    Submit
+                </Button>
+                <Button
+                    variant="outlined"
+                    color="secondary"
+                    size="small"
+                    fullWidth
+                    onClick={handleClear}
+                >
+                    Clear
+                </Button>
+            </form>
+        </Paper>
+    );
 };
 
 export default Form;
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import Post from './Post/Post';
 import useStyles from './styles';
-
+import { useSelector } from 'react-redux';
+import { Grid, CircularProgress } from '@material-ui/core';
 const Posts = () => {
     const classes = useStyles();
-    return (
-        <>
-            <h1 className={classes.post}>Posts</h1>
-            <Post />
-            <Post />
-        </>
+    const posts = useSelector((state) => state.posts);
+    console.log(posts);
+    return !posts.length ? (
+        <CircularProgress />
+    ) : (
+        <Grid
+            container
+            className={classes.container}
+            alignItems="stretch"
+            spacing={3}
+        >
+            {posts.map((post) => (
+                <Grid item key={post._id} xs={12} sm={6}>
+                    <Post post={post} />
+                </Grid>
+            ))}
+        </Grid>
     );
 };
 
